cd05c41f56deefff38fab5c96ee001edb46717a8
Mai Anh Bao <chuyendungdangki@yahoo.com>
2017-01-10 11:42:13 +0700

    euler cpp

---
+++ EulerCpp/EulerModule.cpp
@@ -0,0 +1,166 @@
+/*
+ * EulerModule.cpp
+ *
+ *  Created on: 5 janv. 2017
+ *      Author: hnnguyen
+ */
+
+#include "EulerModule.h"
+
+using namespace EulerProject;
+
+/**
+ * @brief check a number is prime or not
+ * @param a positive integer number
+ * @return true if the number is prime
+ * @return false if the number is not prime
+ */
+bool EulerModule::checkPrime(const int num) throw (NegativeException&) {
+	bool retVal;
+	if (num < 2)
+		return false;
+	else if (num == 2)
+		return true;
+	else {
+		for (int i = 3; i < static_cast<int>(num / 2) + 1; ++i) {
+			if (num % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
+
+/**
+ * @brief find all divisors of a number
+ * @param a positive integer number
+ * @return list of all divisors
+ */
+std::vector<int> EulerModule::getDivisors(const int num)
+		throw (NegativeException&) {
+	if (num < 0) {
+		throw NegativeException("Negative integer!\n");
+	}
+	std::vector<int> retVal;
+	retVal.push_back(1);
+	for (int i = 2; i < num / 2 + 1; ++i) {
+		if (num % i == 0) {
+			retVal.push_back(i);
+		}
+	}
+	retVal.push_back(num);
+	return retVal;
+}
+
+/**
+ * @brief find all prime divisors of a number
+ * @param a positive integer number
+ * @return list of all prime divisors
+ */
+std::vector<int> EulerModule::getPrimeDivisors(const int num)
+		throw (NegativeException&) {
+	if (num < 0) {
+		throw NegativeException("Negative integer!\n");
+	}
+	std::vector<int> retVal;
+	int divisor = 3;
+	int iNum = num;
+	if (iNum % 2 == 0) {
+		retVal.push_back(2);
+		while (iNum % 2 == 0) {
+			iNum = iNum / 2;
+		}
+	}
+
+	while (iNum > 1) {
+		if (iNum % divisor == 0) {
+			retVal.push_back(divisor);
+			while (iNum % divisor == 0) {
+				iNum = iNum / divisor;
+			}
+		} else {
+			divisor += 2;
+			while (not checkPrime(divisor)) {
+				divisor += 2;
+			}
+		}
+	}
+	return retVal;
+}
+
+/**
+ * @brief factorize a number
+ * @param a positive integer number
+ * @return list of divisors
+ */
+std::vector<int> EulerModule::factorize(const int num)
+		throw (NegativeException&) {
+	if (num < 0) {
+		throw NegativeException("Negative integer!\n");
+	}
+	std::vector<int> retVal;
+	if (num == 1) {
+		retVal.push_back(num);
+	} else {
+		int iNum = num;
+		int divisor = 3;
+		while (iNum % 2 == 0) {
+			retVal.push_back(2);
+			iNum = iNum / 2;
+		}
+		while (iNum > 1) {
+			if (iNum % divisor == 0) {
+
+				while ((iNum % divisor) == 0) {
+					retVal.push_back(divisor);
+					iNum = iNum / divisor;
+				}
+			} else {
+				divisor += 2;
+			}
+		}
+	}
+	return retVal;
+}
+
+/**
+ * @brief print the vector of integers
+ * @param vector of integers
+ * @return none
+ */
+void EulerModule::printResult(std::vector<int> vecRes) {
+	std::cout << "[";
+	for (auto& x : vecRes) {
+		std::cout << x << " ";
+	}
+	std::cout << "]" << std::endl;
+}
+
+/**
+ * @brief generate list of all prime numbers which is smaller than a given number
+ * @param a positive integer number
+ * @return list of all prime number
+ */
+std::vector<int> EulerModule::sieveEratosthenes(const int num)
+		throw (NegativeException&) {
+	if (num < 0) {
+		throw NegativeException("Negative integer!\n");
+	}
+	std::vector<int> retVal;
+	std::vector<int> vecInitial(num, 1);
+	int upper = static_cast<int>(sqrt(num));
+	for (int i = 2; i < upper; ++i) {
+		if (vecInitial.at(i)) {
+			std::cout << i;
+			for (int j = i * i; j < num; j += i) {
+				vecInitial.at(j) = 0;
+			}
+		}
+	}
+	for (int i = 0; i < num; ++i) {
+		if (vecInitial.at(i)) {
+			retVal.push_back(i);
+		}
+	}
+	return retVal;
+}
---
+++ EulerCpp/EulerModule.h
@@ -0,0 +1,44 @@
+/*
+ * EulerModule.h
+ *
+ *  Created on: 5 janv. 2017
+ *      Author: hnnguyen
+ */
+
+#ifndef EULERMODULECPP_EULERMODULE_H_
+#define EULERMODULECPP_EULERMODULE_H_
+
+#include <math.h>
+#include <iostream>
+#include <vector>
+#include <exception>
+
+namespace EulerProject {
+
+class NegativeException: public std::exception {
+public:
+	NegativeException(const char* errMessage):errMessage_(errMessage){}
+	virtual const char* what() const throw() {
+		return errMessage_;
+	}
+
+private:
+	const char* errMessage_;
+};
+
+class EulerModule {
+public:
+	static bool checkPrime(const int) throw(NegativeException&);
+	static std::vector<int> getDivisors(const int) throw(NegativeException&);
+	static std::vector<int> getPrimeDivisors(const int) throw(NegativeException&);
+	static std::vector<int> factorize(const int) throw(NegativeException&);
+	static std::vector<int> sieveEratosthenes(const int) throw(NegativeException&);
+
+	static void printResult(std::vector<int>);
+
+//	NegativeException negativeException;
+};
+
+} /* namespace EulerProject */
+
+#endif /* EULERMODULECPP_EULERMODULE_H_ */
